e6f7d56ccb57afc4642aedae2dcc5960c145b1e1
Bassel El-Sheikh <belsheikh@ejada.com>
2025-11-09 18:01:29 +0300

    .

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, request, session, url_for
+from msal import ConfidentialClientApplication
 from flask_login import current_user, login_user, logout_user, login_required
 import msal
 import uuid
@@ -13,14 +14,15 @@ from FlaskExercise.models import User
 def home():
     return render_template('index.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     session['state'] = str(uuid.uuid4())
     # Note: Below will return None as an auth_url until you implement the function
-    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session['state'])
+    cache = _load_cache()
+    msal_app = _build_msal_app(cache=cache)
+    auth_url = _build_auth_url(msal_app=msal_app, state=session['state'])
     return render_template('login.html', title='Sign In', auth_url=auth_url)
 
 
@@ -32,7 +34,10 @@ def logout():
         session.clear()
         # TODO: Also logout from your tenant's web session
         #   And make sure to redirect from there back to the login page
-        pass
+        return redirect(
+            Config.AUTHORITY + "/oauth2/v2.0/logout" +
+            "?post_logout_redirect_uri=" + url_for("login", _external=True)
+        )
 
     return redirect(url_for('login'))
 
@@ -47,7 +52,12 @@ def authorized():
         cache = _load_cache()
         # TODO: Acquire a token by authorization code from an MSAL app
         #  And replace the error dictionary
-        result = {'error': 'Not Implemented', 'error_description': 'Function not implemented.'}
+        msal_app = _build_msal_app(cache)
+        code = request.args.get("code")
+        result = msal_app.acquire_token_by_authorization_code(
+            code,
+            scopes=Config.SCOPE
+        )
         if 'error' in result:
             return render_template('auth_error.html', result=result)
         session['user'] = result.get('id_token_claims')
@@ -73,11 +83,19 @@ def _save_cache(cache):
         session['token_cache'] = cache.serialize()
 
 
-def _build_msal_app(cache=None, authority=None):
+def _build_msal_app(cache=None):
     # TODO: Create and return a Confidential Client Application from msal
-    return None
+    return ConfidentialClientApplication(
+        client_id=Config.CLIENT_ID,
+        authority=Config.AUTHORITY,
+        client_credential=Config.CLIENT_SECRET,
+        token_cache=cache
+    )
 
 
-def _build_auth_url(authority=None, scopes=None, state=None):
+def _build_auth_url(msal_app: ConfidentialClientApplication,  state=None):
     # TODO: Get the authorization request URL from a built msal app, and return it
-    return None
+    return msal_app.get_authorization_request_url(
+        Config.SCOPE,
+        redirect_uri="test-entra-id-app.azurewebsites.net"+ Config.REDIRECT_PATH
+    )
